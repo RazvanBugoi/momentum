43c7888bbf61cd6105bb1a4703e3441891298a41
RazvanBugoi <razvanbugoi@gmail.com>
2020-09-08 15:02:54 +0100

    added function that gets the current time to DOM in JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,6 @@
-console.log('test')
+let time = document.getElementById('time');
+
+
+let currentTime = new Date();
+let localTime = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
+time.innerHTML = localTime;
