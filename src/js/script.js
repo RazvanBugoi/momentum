76e9c97328319c6490556d308b78fb3047ed5a6e
RazvanBugoi <razvanbugoi@gmail.com>
2020-09-09 13:03:47 +0100

    removed border top, right and left from input field

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,13 @@
 let timeContainer = document.getElementById('time');
+let inputField = document.getElementById('task');
 
 
 let currentTime = new Date();
 let localTime = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
-time.innerHTML = localTime;
+timeContainer.innerHTML = localTime;
+
+// inputField.onclick = changeBorder;
+
+// function changeBorder() {
+//     inputField.style.border = '2px solid #FF9800';
+// }
