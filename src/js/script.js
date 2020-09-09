26621e12370e98892c26b4aac4287625f1d840d7
RazvanBugoi <razvanbugoi@gmail.com>
2020-09-09 14:12:05 +0100

    changed border bottom colour for input field to white

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,7 @@
 let timeContainer = document.getElementById('time');
 let inputField = document.getElementById('task');
-
+let listOfTodos = document.getElementById('todoItems');
 
 let currentTime = new Date();
 let localTime = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
 timeContainer.innerHTML = localTime;
-
-// inputField.onclick = changeBorder;
-
-// function changeBorder() {
-//     inputField.style.border = '2px solid #FF9800';
-// }
