708415cb9711e54a18c01f2fef7f57a4875cd2fb
RazvanBugoi <razvanbugoi@gmail.com>
2020-09-10 20:02:07 +0100

    added event listener to input field when Enter key is pressed

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,9 @@ let listOfTodos = document.getElementById('todoItems');
 let currentTime = new Date();
 let localTime = `${currentTime.getHours()}:${currentTime.getMinutes()}`;
 timeContainer.innerHTML = localTime;
+
+inputField.addEventListener("keypress", function onEvent(event) {
+    if (event.key === 'Enter') {
+        console.log(inputField.value);
+    }
+})
